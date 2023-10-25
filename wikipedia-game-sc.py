b8a0f6ae17216221dfe373a477fb47e5a26eac52
Mallika Sheshadri <mallika.n.sheshadri@gmail.com>
2023-10-25 19:14:34 +0000

    donedone

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -21,24 +21,47 @@ def wikipedia_game_solver(start_page, target_page):
     start_time = time.time()
   
     visited = []
-    queue = Queue[]
-    path = 1
+    queue = Queue()
+    path = []
+    parent={}
 
+
+    queue.put(start_page.title)
+    visited.append(start_page.title)
+
+
+    while not queue.empty():
+
+        current_title = queue.get()
+
+        if current_title == target_page.title:
+            break
+        #visit my current_title
+        visited.append(current_title)
+        current_page = wiki_wiki.page(current_title)
+        next_level = fetch_links(current_page)
+
+        for node in next_level:
+             if node not in visited:
+                 queue.put(node)
+                 parent[node] = current_title
+
+    child = target_page.title
+    while child != start_page.title:
+        path.append(child)
+        child = parent[child]
+    path.append(start_page.title)
+    path.reverse()
     end_time = time.time()
     print("This algorithm took", end_time-start_time, "seconds to run!")
 
-    while not queue.empty():
-        #get from queue (queue.get())
-        #add to visited
-        #fetch the links
-        #if my thing is in there, break
-        #else, continue
-  
     return path
 
+
+
 # Example usage:
-start_page = wiki_wiki.page('Nina Tandon')
-target_page = wiki_wiki.page('Italian language')
+start_page = wiki_wiki.page('thumb')
+target_page = wiki_wiki.page('finger')
 path = wikipedia_game_solver(start_page, target_page)
 print("Shortest path:", path)
 
